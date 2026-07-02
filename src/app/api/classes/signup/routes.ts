a9183046a5a779c3d1417f6222ac12596fc03d1e
Pat Kelsh <pkelsh@fearless.tech>
2026-07-02 12:38:47 -0400

    Update routes.ts with comments

--- src/app/api/classes/signup/routes.ts
+++ src/app/api/classes/signup/routes.ts
@@ -5,7 +5,7 @@ import { createRosterEntry, deleteRosterEntriesByUserAndClass } from "@/lib/pris
 import { getUserById } from "@/lib/prisma/user";
 import { sendNotificationEmail } from "@/helpers/email/notification";
 
-// Create a new roster entry for the signed in user for a class. Will create the roster entry and create a notification email
+// Create a new roster entry for the signed in user for a class. Will create the roster entry and create a notification email. Request will include the class id to validate the sign up
 export async function POST(request: NextRequest) {
     const session = await getSession();
     const user = session?.user;
@@ -39,7 +39,8 @@ export async function POST(request: NextRequest) {
     }
 }
 
-// Remove the signed in user from the class roster. This will delete the roster entry for the user and class combination. The user must be signed in to perform this action.
+// Remove the signed in user from the class roster. Request must include the RosterItem ID to delete the class sign up
+// This will delete the roster entry for the user and class combination. The user must be signed in to perform this action.
 export async function DELETE(request: NextRequest) {
     const session = await getSession();
     const user = session?.user;
