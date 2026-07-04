559ead33b58b47970078fd312b038f3016f30393
Nicole Zonnenberg <nicolezonnenberg@Nicoles-MacBook-Pro.local>
2026-07-04 13:49:54 -0400

    backtrack location modal

--- src/components/modals/AddLocation.tsx
+++ src/components/modals/AddLocation.tsx
@@ -4,19 +4,13 @@ import { Dispatch, SetStateAction, SubmitEvent, useState } from "react";
 import AddIcon from '@mui/icons-material/Add';
 import Button from "@/components/.ui/Button";
 import ModalComponent from "@/components/.ui/Modal";
-
-import LocationForm from "@/components/forms/location";
-
-interface LocationFormData {
-  name: string;
-  type: "Indoor" | "Outdoor" | "Online";
-  notes?: string;
-}
+import Select from "@/components/.ui/Select";
+import TextField from "@/components/.ui/TextField";
 
 
 const AddLocationModal = ({ setIsLoading }: { setIsLoading: Dispatch<SetStateAction<boolean>> }) => {
   const [closeOnAction, setCloseOnAction] = useState(false);
-  const [formData, setFormData] = useState<LocationFormData>({
+  const [formData, setFormData] = useState({
     name: "",
     type: "Indoor",
     notes: "",
@@ -79,8 +73,7 @@ const AddLocationModal = ({ setIsLoading }: { setIsLoading: Dispatch<SetStateAct
       closeOnAction={closeOnAction}
     >
       <div className="form-container modal-form">
-        <LocationForm onChange={handleInputChange} initialData={formData} />
-        {/* <form id="location-form">
+        <form id="location-form">
           <div className="form-row-two-thirds">
             <TextField
               label="Space Name"
@@ -107,7 +100,7 @@ const AddLocationModal = ({ setIsLoading }: { setIsLoading: Dispatch<SetStateAct
             onChange={handleInputChange}
             formHelperText
           />
-        </form> */}
+        </form>
       </div>
     </ModalComponent>
   )
